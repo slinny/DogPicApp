fb9c5d908ac72265e0d03d11f3bbc6388676ceb0
slinny <siranli411@gmail.com>
2018-05-20 14:59:50 -0400

    login activity

---
+++ app/src/main/java/nyc/c4q/finalpracticalretake/LoginActivity.java
@@ -0,0 +1,73 @@
+package nyc.c4q.finalpracticalretake;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static nyc.c4q.finalpracticalretake.util.Constants.DEFAULT_USER;
+import static nyc.c4q.finalpracticalretake.util.Constants.SHARED_PREFS;
+import static nyc.c4q.finalpracticalretake.util.Constants.USER_KEY;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameET, passwordET;
+    private String username;
+    private Button submitBtn;
+    private SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        checkUser();
+        setViews();
+        setButtonOnClick();
+    }
+
+    private void checkUser() {
+        preferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        username = preferences.getString(USER_KEY, DEFAULT_USER);
+        if (!username.equalsIgnoreCase(DEFAULT_USER)) {
+            startActivity(new Intent(LoginActivity.this, BreedsActivity.class));
+            finish();
+        }
+    }
+
+    private void setViews(){
+        usernameET = findViewById(R.id.username_edittext);
+        passwordET = findViewById(R.id.password_edittext);
+        submitBtn = findViewById(R.id.submit_button);
+    }
+
+    private void setButtonOnClick(){
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (usernameET.getText().toString().equals("") || passwordET.getText().toString().equals("")) {
+                    if (usernameET.getText().toString().equals("")) {
+                        usernameET.setHint(R.string.enter_username);
+                    } else if (passwordET.getText().toString().equals("")) {
+                        passwordET.setHint(R.string.enter_password);
+                    }
+                } else if (usernameET.getText() != null && passwordET.getText() != null) {
+                    username = usernameET.getText().toString();
+                    if (passwordET.getText().toString().toLowerCase().contains(username.toLowerCase())) {
+                        passwordET.setText(R.string.contains_username);
+                    } else {
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString(USER_KEY, username).apply();
+                        startActivity(new Intent(LoginActivity.this, BreedsActivity.class));
+                    }
+                }
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/nyc/c4q/finalpracticalretake/util/Constants.java
@@ -0,0 +1,17 @@
+package nyc.c4q.finalpracticalretake.util;
+
+/**
+ * Created by C4Q on 5/20/18.
+ */
+
+public class Constants {
+    public static final String SHARED_PREFS = "shared_preferences";
+    public static final String USER_KEY = "username_key";
+    public static final String DEFAULT_USER = "username";
+    public static final String BREED_NAME = "breed_name";
+    public static final String IMAGE_URL = "image_url";
+    public static final String TERRIER = "terrier";
+    public static final String SPANIEL = "spaniel";
+    public static final String RETRIEVER = "retriever";
+    public static final String POODLE = "poodle";
+}
